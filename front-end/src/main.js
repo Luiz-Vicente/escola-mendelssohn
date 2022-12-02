50a25f68aa0ab00d0fb32a2f99a86bace8c3c543
Luiz Fernando <fernandoluiz43673@gmail.com>
2022-12-02 15:52:54 -0300

    vercel analytics installed and sitemap created
    Signed-off-by: Luiz Vicente <dev.vicente.contato@gmail.com>

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import { inject } from "@vercel/analytics";
 
 import "@/assets/scss/main.scss";
 const app = createApp(App);
@@ -8,3 +9,5 @@ const app = createApp(App);
 app.use(router);
 
 app.mount("#app");
+
+inject();
